ec09b7dc458bf260f1798cdedeb89226a896a933
username <cuopbien119@gmail.com>
2020-07-28 21:53:27 -0400

    changing map coordinates

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 function myMap() {
   var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(27.753099,-82.638113); 
+  var myCenter = new google.maps.LatLng(27.752876,-82.638033); 
   var mapOptions = {center: myCenter, zoom: 13};
   var map = new google.maps.Map(mapCanvas,mapOptions);
   var marker = new google.maps.Marker({
